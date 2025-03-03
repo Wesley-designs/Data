6a8aa2f472d6fdf8547b523cb959a09899177047
Wesley-designs <jiewesleyd2789@nycstudents.net>
2025-03-03 14:31:03 +0000

    sfhaoghyrhg

--- Data.java
+++ Data.java
@@ -1,6 +1,6 @@
 public class Data
 {
-public static final int MAX = /* value not shown */;
+//public static final int MAX = /* value not shown */;
 private int[][] grid;
 /** Fills all elements of grid with randomly generated values, as described in part (a)
 * Precondition: grid is not null.
@@ -8,8 +8,18 @@ private int[][] grid;
 */
 public void repopulate()
 {
-    for (int i=0; i<Math.random()*10; i++) {
-        
+    int hori = (int)Math.random()*10;
+    int vert = (int)Math.random()*10;
+    grid = new int[hori][vert];
+
+    for (int i=0; i<vert; i++) {
+        for (int o=0; i<hori; i++) {
+            int randnum = 100;
+            while ((randnum%100 == 0) || (randnum%10!=0)) {
+                randnum = (int)((Math.random()*10)*(Math.random()*10)*10);
+            }
+            grid[i][o] = randnum;
+        }
     }
 }
 /** Returns the number of columns in grid that are in increasing order, as described
@@ -18,6 +28,18 @@ public void repopulate()
 * grid has at least one element.
 */
 public int countIncreasingCols()
-{ /* to be implemented in part (b) */ }
+{
+    int count = 0;
+    int h = grid.length;
+    int y = grid[0].length;
+    for (int i=0; i<h-1; i++) {
+        for (int o=0; i<v; i++) {
+            if (grid[i][o]>=grid[i+1][o]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
 // There may be instance variables, constructors, and methods that are not shown.
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Data a = new Data;
+        Data a = new Data();
+        a.repopulate();
+        System.out.println(a.countIncreasingCols());
     }
 }
